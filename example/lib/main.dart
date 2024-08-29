b414c3de206c326353b0d6f5695c8c6727652bd7
hafiz <hafizm.huzaifa1234gf@gmail.com>
2024-08-29 18:12:45 +0500

    first commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -100,7 +100,7 @@ class _MyAppState extends State<MyApp> {
   Future loadModel() async {
     Tflite.close();
     try {
-      String res;
+      String? res;
       switch (_model) {
         case yolo:
           res = await Tflite.loadModel(
